e3655c7653ac3a9e38bdfe66a7ec71253fd9c6ed
Agnesa Muradyan <agnesmuradyan@gmail.com>
2026-07-18 19:47:30 +0400

    add tg bot

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'myapp',
+    'myapp.apps.MyappConfig',
     'rest_framework',
     'webpack_loader',
     'corsheaders',
@@ -68,6 +68,7 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'myapp.middleware.CurrentUserMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'myapp.middleware.XFrameOptionsMiddleware'
 ]
@@ -169,3 +170,7 @@ REST_FRAMEWORK = {
         "rest_framework.filters.OrderingFilter",
     ],
 }
+
+TELEGRAM_BOT_TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN", "")
+TELEGRAM_CHANNEL_ID = os.environ.get("TELEGRAM_CHANNEL_ID", "")
+TELEGRAM_NOTIFICATIONS_ENABLED = env.bool("TELEGRAM_NOTIFICATIONS_ENABLED", default=True)
--- backend/myapp/middleware.py
+++ backend/myapp/middleware.py
@@ -1,4 +1,24 @@
 from django.utils.deprecation import MiddlewareMixin
+from threading import local
+
+
+_request_local = local()
+
+
+def get_current_user():
+    return getattr(_request_local, "user", None)
+
+
+class CurrentUserMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        _request_local.user = getattr(request, "user", None)
+        try:
+            return self.get_response(request)
+        finally:
+            _request_local.user = None
 
 
 class XFrameOptionsMiddleware(MiddlewareMixin):
